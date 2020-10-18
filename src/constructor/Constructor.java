ef571045e2196d683f3f3250aefe64d0ff504ea6
arjun-sudo <laserarjun@gmail.com>
2020-10-18 10:34:02 +0545

    default constructor

---
+++ src/constructor/Constructor.java
@@ -0,0 +1,18 @@
+package constructor;/*
+ * Created By Arjun Gautam .
+ * Date : 10/18/20
+ * Time : 10:30 AM
+ */
+//Java program to create and call a default constructor
+public class Constructor {
+    Constructor(){
+        System.out.println("Constructor is created");
+
+    }
+
+    public static void main(String[] args) {
+        Constructor obj=new Constructor();
+    }
+}
+//Q) What is the purpose of a default constructor?
+//        The default constructor is used to provide the default values to the object like 0, null, etc., depending on the type
