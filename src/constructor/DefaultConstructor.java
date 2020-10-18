17ef0aa257d04d2b0ae313333962ea6ac30c8b94
arjun-sudo <laserarjun@gmail.com>
2020-10-18 10:38:51 +0545

    default constructor

---
+++ src/constructor/DefaultConstructor.java
@@ -0,0 +1,14 @@
+package constructor;/*
+ * Created By Arjun Gautam .
+ * Date : 10/18/20
+ * Time : 10:34 AM
+ */
+//Java program demonstrating default constructor
+public class DefaultConstructor {
+    int id;
+    String name;
+
+    void display()
+
+
+}
