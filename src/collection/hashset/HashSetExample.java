5006afa8041f242c10917413b2487c4512c04789
arjun-sudo <arjungautam5431@gmail.com>
2020-12-06 14:32:14 +0545

    Hashset example

--- src/collection/hashset/HashSetExample.java
+++ src/collection/hashset/HashSetExample.java
@@ -6,6 +6,25 @@
  */
 package collection.hashset;
 
+import java.util.HashSet;
+import java.util.Iterator;
+
 public class HashSetExample {
+
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<>();
+        set.add("Ravi");
+        set.add("Vijay");
+        set.add("Ravi");
+        set.add("Ajay");
+
+        //Traversing elements
+        Iterator<String> iterator = set.iterator();
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+    }
 
 }
