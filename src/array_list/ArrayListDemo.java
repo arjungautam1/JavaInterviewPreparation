a7dc264442d29115aa6a6385a905e8e7539ef14c
arjun-sudo <arjungautam5431@gmail.com>
2020-12-06 12:08:21 +0545

    ArrayList

---
+++ src/array_list/ArrayListDemo.java
@@ -0,0 +1,28 @@
+/**
+ * Created By Arjun Gautam .
+ * Date: 2020-12-06
+ * Time: 12:01
+ * Project Name : JavaEssential
+ */
+package array_list;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<>();
+        for (int i = 1; i <= 5; i++) {
+            al.add(i);
+        }
+        System.out.println(al);
+
+        al.remove(3);
+        System.out.println(al);
+
+        for (int i = 0; i < al.size(); i++) {
+            System.out.print(al.get(i) + " ");
+        }
+
+
+    }
+}
