dc4d0f9b3b7aa3293b211f6e0e42fa4eaf1ef846
arjun-sudo <arjungautam5431@gmail.com>
2020-12-09 12:43:34 +0545

    Throw exception

--- src/exception/ThrowExample.java
+++ src/exception/ThrowExample.java
@@ -5,18 +5,27 @@
  * Project Name : JavaEssential
  */
 package exception;
+/*Throw keyword us used to explictly throw an exception from a method or any block of code .
+The throw keyword is mainly used to throw custom exception */
+//        Java program to demostrate the use of throw
+
+
+import java.util.Scanner;
 
 public class ThrowExample {
+    static void validate(int age) {
+        if (age < 18)
+            throw new ArithmeticException("Not valid ");
+        else
+            System.out.println("Welcome to vote .");
+    }
+
     public static void main(String[] args) {
+        System.out.println("Enter you age :");
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        validate(age);
+        System.out.println("Rest of the Code.");
 
-
-        int i = 1, j = 10;
-        do {
-            if (i++ > --j) {
-                continue;
-            }
-        }
-        while(i<5);
-        System.out.println("i= "+i + "and j="+j);
     }
 }
